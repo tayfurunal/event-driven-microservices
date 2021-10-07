baffb46a4465ccd81f0d94103feffab6212586cc
tayfurunal <mtayfurunal@gmail.com>
2021-10-07 22:33:28 +0300

    create basket service

---
+++ basket-service/src/main/java/com/tayfurunal/basketservice/BasketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tayfurunal.basketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketServiceApplication.class, args);
+    }
+
+}
